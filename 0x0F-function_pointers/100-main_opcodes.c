908bdc3e5629419e360cfed27508558cdce23c61
Keith Inzai <keithinzai@gmail.com>
2023-10-11 11:52:49 -0700

    final edit  0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -23,16 +23,16 @@ bytes = atoi(argv[1]);
 
 if (bytes < 0)
 {
- printf("Error\n");
- exit(2);
+printf("Error\n");
+exit(2);
 }
 
 arr = (char *)main;
 for (i = 0; i < bytes; i++)
 {
-if(i== bytes - 1)
+if (i == bytes - 1)
 {
-if("%02hhx\n", arr[i]);
+if ("%02hhx\n", arr[i]);
 break;
 }
 	printf("%02hhx ", arr[i]);
